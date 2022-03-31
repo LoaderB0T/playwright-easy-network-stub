7a090749ec65c5b9b794869a5ea46e7c0fdbde15
Janik Schumacher <rndmb0t@gmail.com>
2022-03-31 13:30:10 +0200

    fixed async behavior

--- src/playwright-easy-network-stub.ts
+++ src/playwright-easy-network-stub.ts
@@ -15,29 +15,25 @@ export class PlaywrightEasyNetworkStub extends EasyNetworkStub {
    * @returns A promise that resolves when the stub is ready to use.
    */
   public async init(context: BrowserContext, logLevel?: LogLevel) {
-    return new Promise<void>(resolve => {
-      this.initInternal({
-        failer: error => {
-          if (error instanceof Error) {
-            throw error;
-          } else {
-            throw new Error(error);
-          }
-        },
-        interceptor: (baseUrl, handler) => {
-          context
-            .route(baseUrl, route => {
-              handler({
-                destroy: () => route.abort(),
-                method: route.request().method() as HttpMethod,
-                url: route.request().url(),
-                reply: r => route.fulfill({ status: r.statusCode, body: JSON.stringify(r.body), headers: r.headers })
-              });
-            })
-            .then(() => resolve());
-        },
-        logLevel
-      });
+    return this.initInternal({
+      failer: error => {
+        if (error instanceof Error) {
+          throw error;
+        } else {
+          throw new Error(error);
+        }
+      },
+      interceptor: (baseUrl, handler) => {
+        return context.route(baseUrl, async route => {
+          await handler({
+            destroy: () => route.abort(),
+            method: route.request().method() as HttpMethod,
+            url: route.request().url(),
+            reply: r => route.fulfill({ status: r.statusCode, body: JSON.stringify(r.body), headers: r.headers })
+          });
+        });
+      },
+      logLevel
     });
   }
 }
